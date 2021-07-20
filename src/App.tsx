d9c17a171c69a5510a18c58af9fd814e9372e7c0
acccdang <acccdang8659@gmail.com>
2021-07-21 00:32:52 +0900

    Add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Roboto&display=swap');
+body {
+  background: #e9ecef;
+  font-family: 'Noto Sans KR', sans-serif;
+}`;
 
 function App() {
   return (
